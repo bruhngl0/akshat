63ec093a6543badfdbb9440ee614857ae9c3d6e4
bruh.ngl0 <adityashrm500@gmail.com>
2024-05-28 12:20:55 +0530

    shape

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Shop = () => {
   return (
+    <>
     <div className='shop'>
      
      <a href='https://www.facebook.com'>
@@ -21,6 +22,34 @@ const Shop = () => {
         <img src = "shades.png"></img>  
       </div>
     </div>
+
+
+<div className='shop-mobile'>
+     
+<a href='https://www.facebook.com'>
+<h1>PENDANT SETS</h1>
+</a>
+ <div className='shop-one'> 
+   <img src= "shades.png"></img>
+   <img src = "shades.png"></img>
+ 
+
+ </div>
+
+
+ <div className='shop-two'> 
+    <img src = "shades.png"></img>
+   <img src = "shades.png"></img>
+  
+ </div>
+
+ <div className='shop-two'> 
+    <img src = "shades.png"></img>
+   <img src = "shades.png"></img>
+    
+ </div>
+</div>
+</>
   )
 }
 
--- src/components/Shop0ne.jsx
+++ src/components/Shop0ne.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const ShopOne = () => {
   return (
+    <>
     <div className='shop-neck'>
 
      <a href='https://www.facebook.com'>
@@ -21,6 +22,34 @@ const ShopOne = () => {
         <img src = "shades6.png"></img>  
       </div>
     </div>
+
+
+<div className='shop-mobile'>
+     
+     <a href='https://www.facebook.com'>
+     <h1>PENDANT SETS</h1>
+     </a>
+      <div className='shop-one'> 
+        <img src= "shades1.png"></img>
+        <img src = "shades2.png"></img>
+      
+     
+      </div>
+     
+     
+      <div className='shop-two'> 
+         <img src = "shades3.png"></img>
+        <img src = "shades4.png"></img>
+       
+      </div>
+     
+      <div className='shop-two'> 
+         <img src = "shades5.png"></img>
+        <img src = "shades6.png"></img>
+         
+      </div>
+     </div>
+     </>
   )
 }
 
