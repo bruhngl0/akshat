da4c311b42e6a319589af86bd4f5a6a58c07bd09
bruh.ngl0 <adityashrm500@gmail.com>
2024-06-09 11:37:39 +0530

    lazy

--- src/components/VideoMobile.jsx
+++ src/components/VideoMobile.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
+import React, { Suspense } from 'react';
+
+const LazyReactPlayer = React.lazy(() => import('react-player'));
 
 const VideoMobile = () => {
   return (
     <div className='videoMobile-main'>
-
-      <ReactPlayer 
-         url="shines.mp4" 
-         playing 
-         width= "100%"
-         height="100%"
-         loop
-         muted
-         controls={false}
-         playsinline // Add this attribute
-         autoPlay // Add this attribute
-         className="videoMobile-main-one"
-    
-      />
+      <Suspense fallback={<div>Loading video...</div>}>
+        <LazyReactPlayer
+          url="shines.mp4"
+          playing
+          width="100%"
+          height="100%"
+          loop
+          muted
+          controls={false}
+          playsinline
+          autoPlay
+          className="videoMobile-main-one"
+        />
+      </Suspense>
     </div>
   );
 };
